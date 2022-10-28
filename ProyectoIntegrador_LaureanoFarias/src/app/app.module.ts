bc953feea59d7a232c40b1b9e14b52629738ebab
maxiluck <laurifarias2004@gmail.com>
2022-10-28 02:41:12 -0300

    creación del header

--- ProyectoIntegrador_LaureanoFarias/src/app/app.module.ts
+++ ProyectoIntegrador_LaureanoFarias/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialesComponent } from './components/sociales/sociales.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LogoAPComponent,
+    SocialesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
