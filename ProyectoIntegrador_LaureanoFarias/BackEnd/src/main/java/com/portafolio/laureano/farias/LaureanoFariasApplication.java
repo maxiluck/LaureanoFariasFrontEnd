803a5fb7c65f8aaa875300d198789047f866de72
maxiluck <laurifarias2004@gmail.com>
2022-12-24 21:36:45 -0300

    dependencias backend

---
+++ ProyectoIntegrador_LaureanoFarias/BackEnd/src/main/java/com/portafolio/laureano/farias/LaureanoFariasApplication.java
@@ -0,0 +1,13 @@
+package com.portafolio.laureano.farias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaureanoFariasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaureanoFariasApplication.class, args);
+	}
+
+}
---
+++ ProyectoIntegrador_LaureanoFarias/BackEnd/src/test/java/com/portafolio/laureano/farias/LaureanoFariasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portafolio.laureano.farias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaureanoFariasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
