9c29f9fd21b09efa3accb31361f316babf2dc939
maxiluck <laurifarias2004@gmail.com>
2023-01-12 16:52:42 -0300

    deje el front

---
+++ src/app/model/persona.model.ts
@@ -0,0 +1,17 @@
+export class persona{
+    id?: number;
+    nombre: string;
+    apellido: string;
+    descripcion:string;
+    img: string;
+
+
+    constructor(nombre: string,apellido: string,descripcion:string, img: string){
+        this.nombre= nombre;
+        this.apellido = apellido;
+        this.img = img;
+        this.descripcion = descripcion;
+    }
+
+
+}
